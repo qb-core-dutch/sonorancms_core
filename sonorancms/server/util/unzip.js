491b766857959d92810087d9c2268699e82262da
Jordan.#2139 <jordansilva213@gmail.com>
2023-04-11 14:02:07 -0400

    Add gitkeep and logic to make addonupdates folder

--- sonorancms/server/util/unzip.js
+++ sonorancms/server/util/unzip.js
@@ -3,20 +3,24 @@ var fs = require("fs");
 
 exports('UnzipFile', (file, dest, type) => {
 	if (type === "core") {
-    try {
-		fs.createReadStream(file).pipe(unzipper.Extract({ path: dest}).on('close', () => {
-			exports[GetCurrentResourceName()].unzipCoreCompleted(true);
-		}))
-	} catch(ex) {
-		exports[GetCurrentResourceName()].unzipCoreCompleted(false, ex);
+		try {
+			fs.createReadStream(file).pipe(unzipper.Extract({ path: dest }).on('close', () => {
+				exports[GetCurrentResourceName()].unzipCoreCompleted(true);
+			}))
+		} catch (ex) {
+			exports[GetCurrentResourceName()].unzipCoreCompleted(false, ex);
+		}
+	} else {
+		try {
+			fs.createReadStream(file).pipe(unzipper.Extract({ path: dest }).on('close', () => {
+				exports[GetCurrentResourceName()].unzipAddonCompleted(true, 'nil', type);
+			}))
+		} catch (ex) {
+			exports[GetCurrentResourceName()].unzipAddonCompleted(false, ex, type);
+		}
 	}
-} else {
-    try {
-		fs.createReadStream(file).pipe(unzipper.Extract({ path: dest}).on('close', () => {
-			exports[GetCurrentResourceName()].unzipAddonCompleted(true, 'nil', type);
-		}))
-	} catch(ex) {
-		exports[GetCurrentResourceName()].unzipAddonCompleted(false, ex, type);
-	}
-}
 });
+
+exports('makeDir', (path) => {
+	fs.mkdirSync(path, { recursive: true })
+})
