71945a53bfc54a0cf627755c5fee8c7119aa24c1
Jordan.#2139 <jordansilva213@gmail.com>
2023-06-15 23:13:33 -0400

    Add in passengers

--- sonorancms/client/client.lua
+++ sonorancms/client/client.lua
@@ -1,14 +1,28 @@
 RegisterNetEvent('SonoranCMS::core::RequestGamePool', function()
 	local returnVehicleData = {}
-	for k, v in pairs(GetGamePool('CVehicle')) do
-		local vehicleData = {}
-		vehicleData.model = GetEntityModel(v)
-		vehicleData.plate = GetVehicleNumberPlateText(v)
-		vehicleData.health = GetVehicleEngineHealth(v)
-		vehicleData.fuel = GetVehicleFuelLevel(v)
-		vehicleData.bodyHealth = GetVehicleBodyHealth(v)
-		vehicleData.displayName = GetDisplayNameFromVehicleModel(GetEntityModel(v))
-        table.insert(returnVehicleData, vehicleData)
+	for _, v in pairs(GetGamePool('CVehicle')) do
+		local ped = GetPedInVehicleSeat(v, -1)
+		if (DoesEntityExist(ped)) and (IsPedAPlayer(ped)) then
+			local vehicleData = {}
+			vehicleData.model = GetEntityModel(v)
+			vehicleData.plate = GetVehicleNumberPlateText(v)
+			vehicleData.health = GetVehicleEngineHealth(v)
+			vehicleData.fuel = GetVehicleFuelLevel(v)
+			vehicleData.bodyHealth = GetVehicleBodyHealth(v)
+			vehicleData.displayName = GetDisplayNameFromVehicleModel(GetEntityModel(v))
+			vehicleData.driver = GetPlayerServerId(ped)
+			vehicleData.passengers = {}
+			for i = -1, GetVehicleMaxNumberOfPassengers(GetVehiclePedIsIn(GetPlayerPed(-1), false)) + 1, 1 do
+				local pedPass = GetPedInVehicleSeat(GetVehiclePedIsIn(GetPlayerPed(-1), false), i)
+				if (DoesEntityExist(pedPass)) then
+					if (IsPedAPlayer(pedPass) and ped ~= pedPass) then
+						local pedServerId = GetPlayerServerId(NetworkGetPlayerIndexFromPed(pedPass))
+						table.insert(vehicleData.passengers, pedServerId)
+					end
+				end
+			end
+			table.insert(returnVehicleData, vehicleData)
+		end
 	end
-    TriggerServerEvent('SonoranCMS::core::ReturnGamePool', returnVehicleData)
+	TriggerServerEvent('SonoranCMS::core::ReturnGamePool', returnVehicleData)
 end)
