6ef6e747eafc424fa887445c34391d46c0893a02
Jordan.#2139 <65438497+Jordan2139@users.noreply.github.com>
2023-07-28 16:27:03 -0400

    Change the handling of uploaded images

--- sonorancms/server/imageHandler.js
+++ sonorancms/server/imageHandler.js
@@ -1,112 +1,26 @@
-/**
- * Setting basic variables
- */
-const http = require('http');
+// Import the required modules
 const fs = require('fs');
-const { formidable } = require('formidable');
-const savePath = GetResourcePath('qb-inventory') + '/html/images'
-const configFilePath = GetResourcePath(GetCurrentResourceName()) + '/config.lua';
-const luaCode = fs.readFileSync(configFilePath, 'utf8');
 const sizeOf = require('image-size');
-let APIKey = '';
-let port = 3000;
 
-/**
- * Parse the API key from the config file
- * Parse the port from the config file
- */
-const apiKeyPattern = /Config\.APIKey\s*=\s*"([^"]+)"/;
-const match = luaCode.match(apiKeyPattern);
-if (match && match[1]) {
-    const apiKey = match[1];
-    APIKey = apiKey;
-}
-const portPattern = /Config\.imageHandlerPort\s*=\s*"([^"]+)"/;
-const match1 = luaCode.match(portPattern);
-if (match1 && match1[1]) {
-    const filePort = match[1];
-    port = filePort;
-}
+exports('SaveBase64ToFile', function(base64String, filepath, filename) {
+    // Get the base64 image data
+    let base64Image = base64String.split(';base64,').pop();
 
-/**
- * Create the server
- * @param {http.IncomingMessage} req
- * @param {http.ServerResponse} res
- * @returns {void}
- */
-const server = http.createServer((req, res) => {
-    const path = req.url;
-    const method = req.method;
-    if (method === 'POST' && path === '/upload') {
-        const form = formidable({ multiples: true });
-        form.parse(req, (err, fields, files) => {
-            // Handle errors
+    // Get the dimensions of the image
+    let dimensions = sizeOf(Buffer.from(base64Image, 'base64'));
+
+    // Check if the image is 100x100
+    if (dimensions.width === 100 && dimensions.height === 100) {
+        // Save the image
+        fs.writeFile(filepath, base64Image, { encoding: 'base64' }, function(err) {
             if (err) {
-                console.error(err);
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Error parsing form data' }));
-                return;
-            }
-            // Handle invalid request types
-            if (fields.type[0] === 'UPLOAD_ITEM_IMAGE') {
-                if (String(fields.key) !== String(APIKey)) {
-                    res.writeHead(403, { 'Content-Type': 'application/json' });
-                    res.end(JSON.strÏÏingify({ error: 'Invalid API key' }));
-                    return;
-                }
-                if (!files.file) {
-                    res.writeHead(400, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ error: 'No file provided' }));
-                    return;
-                }
-                if (!files.file[0].originalFilename) {
-                    res.writeHead(400, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ error: 'No file provided' }));
-                    return;
-                }
-                if (!files.file[0].originalFilename.endsWith('.png')) {
-                    res.writeHead(400, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ error: 'Invalid file type' }));
-                    return;
-                }
-                const imageSize = sizeOf(files.file[0].filepath);
-                if (imageSize.width !== 100 || imageSize.height !== 100) {
-                    res.writeHead(400, { 'Content-Type': 'application/json' });
-                    res.end(JSON.stringify({ error: 'Invalid image size' }));
-                    return;
-                }
-                const file = files.file[0]; // Access the first file object in the array
-                const saveFilePath = savePath + '/' + file.originalFilename;
-                // Handle invalid file types
-                fs.readFile(file.filepath, (err, data) => { // <-- Corrected property name to "filepath"
-                    if (err) {
-                        console.error(err);
-                        res.writeHead(500, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ error: 'Error reading file' }));
-                        return;
-                    }
-                    fs.writeFile(saveFilePath, data, (err) => {
-                        if (err) {
-                            console.error(err);
-                            res.writeHead(500, { 'Content-Type': 'application/json' });
-                            res.end(JSON.stringify({ error: 'Error writing file' }));
-                            return;
-                        }
-                        res.writeHead(200, { 'Content-Type': 'application/json' });
-                        res.end(JSON.stringify({ success: true }));
-                    });
-                });
+                return {success: false, error: err}
             } else {
-                res.writeHead(400, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ error: 'Invalid request type' }));
+                return {success: true, error: filename}
             }
         });
+    } else {
+        // Return an error
+        return {success: false, error: 'Image must be 100x100'}
     }
-});
-
-/**
- * Start the server
- */
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
 });
--- sonorancms/server/imageHost.lua
+++ sonorancms/server/imageHost.lua
@@ -1,23 +1,49 @@
 --- Image host for SonoranCMS
 --- This is a simple image host for SonoranCMS. It is not required to run the CMS, but is required for the item image feature.
-
 SetHttpHandler(function(req, res)
-    local path = req.path:gsub("/proxy.*", "")
-    local method = req.method
-    if method == 'GET' then
-        local imagePath = GetResourcePath('qb-inventory') .. '/html/' .. path .. '.png'
-        if not path or not imagePath then
-            res.send(json.encode({ error = 'Invalid path' }))
-            return
-        end
-        local file = io.open(imagePath, 'rb')
-        if not file then
-            res.send(json.encode({ error = 'Image not found' }))
-            return
-        else
-            local content = file:read("*all")
-            file:close()
-            res.send(content)
-        end
-    end
+	local path = req.path:gsub('/proxy.*', '')
+	local method = req.method
+	if method == 'GET' then
+		local imagePath = GetResourcePath('qb-inventory') .. '/html/' .. path .. '.png'
+		if not path or not imagePath then
+			res.send(json.encode({error = 'Invalid path'}))
+			return
+		end
+		local file = io.open(imagePath, 'rb')
+		if not file then
+			res.send(json.encode({error = 'Image not found'}))
+			return
+		else
+			local content = file:read('*all')
+			file:close()
+			res.send(content)
+		end
+	end
+	if method == 'POST' then
+		local data = req.body
+		local decoded = json.decode(data)
+		if decoded.data.key ~= Config.APIKey then
+			res.send(json.encode({error = 'Invalid API key'}))
+			return
+		end
+		if not path or path ~= '/upload' then
+			res.send(json.encode({error = 'Invalid path'}))
+			return
+		end
+		if not data then
+			res.send(json.encode({error = 'Invalid data'}))
+			return
+		end
+		if not decoded or not decoded.data.raw then
+			res.send(json.encode({error = 'Invalid data'}))
+			return
+		end
+		exports[GetCurrentResourceName()]:SaveBase64ToFile(decoded.data.raw, GetResourcePath('qb-inventory') .. '/html/' .. decoded.data.name, decoded.data.name, function(success)
+			if success.success then
+				res.send(json.encode({success = true, filename = success.error}))
+			else
+				res.send(json.encode({success = false, error = 'Failed to save image. Error: ' .. success.error}))
+			end
+		end)
+	end
 end)
